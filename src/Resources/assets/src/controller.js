977a58ef39269babafeedb247c8d8bbfd74959d5
stwe <stwe@nomail.tt>
2021-04-06 17:54:48 +0200

    use targets, load first table, load style

--- src/Resources/assets/src/controller.js
+++ src/Resources/assets/src/controller.js
@@ -1,9 +1,18 @@
 'use strict';
 
 import { Controller } from 'stimulus';
+import $ from 'jquery';
+import 'datatables';
 
 export default class extends Controller {
+    static targets = ['sgdatatable'];
+
     connect() {
         console.log("Connect");
+
+        $(this.sgdatatableTarget).DataTable( {
+            scrollY: 300,
+            paging: false
+        } );
     }
 }
